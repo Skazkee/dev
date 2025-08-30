5b372b927fb3e4244e9c49eb237610e52835d705
Skazkee <91323366+Skazkee@users.noreply.github.com>
2025-08-30 15:49:07 +0200

    bot_alert

---
+++ bot.py
@@ -0,0 +1,28 @@
+import os
+from telegram import Update
+from telegram.ext import ApplicationBuilder, MessageHandler, filters, ContextTypes
+
+BOT_TOKEN = os.getenv("8219059346:AAFX8LSVgoNkRRkdQLQp1vDvjAy9zh7A7zs")
+MY_USER_ID = int(os.getenv("427213693", "0"))
+
+KEYWORDS = ["спам", "оплата", "взлом", "жалоба", "продам", "Продам", "Продаю"]
+
+async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    if update.message is None or update.message.text is None:
+        return
+
+    message_text = update.message.text.lower()
+    sender = update.message.from_user
+
+    if any(word in message_text for word in KEYWORDS):
+        alert = (
+            f"🚨 Обнаружено ключевое слово!\n"
+            f"👤 От: {sender.full_name} (@{sender.username})\n"
+            f"🗨 Сообщение: {update.message.text}"
+        )
+        await context.bot.send_message(chat_id=MY_USER_ID, text=alert)
+
+if __name__ == '__main__':
+    app = ApplicationBuilder().token(BOT_TOKEN).build()
+    app.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, handle_message))
+    app.run_polling()
